050eee88ed3b07e5260b354f8d7ee81bac9bdf66
Christopher Holt <holt.christopher1@gmail.com>
2024-03-28 20:23:51 +1100

    set up sorting for the table heads

--- crypto-frontend/src/components/CoinTable.jsx
+++ crypto-frontend/src/components/CoinTable.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LineChart, Line, Tooltip, YAxis } from 'recharts';
 import { IoIosStarOutline } from "react-icons/io";
 import { IoMdStar } from "react-icons/io";
@@ -7,30 +7,60 @@ import { useQuery } from "@tanstack/react-query";
 
 const CoinTable = ({ searchQuery }) => {
     const [activeStates, setActiveStates] = useState({});
-
-    const handleSort = (e) => {
-        const column = e.target.innerText.toLowerCase();
-        const sortedCoins = [...coinsData].sort((a, b) => {
-            if (column === 'name') {
-                return a[column].localeCompare(b[column]);
-            } else {
-                return b[column] - a[column];
-            }
-        });
-        setCoins(sortedCoins);
-    }
-
+    const [coinsData, setCoinsData] = useState([]);
+    const [sortOrder, setSortOrder] = useState({
+        rank: 'asc',
+        name: 'asc',
+        price: 'asc',
+        '24hr': 'asc',
+        volume: 'asc',
+        marketCap: 'asc'
+    });
+    
     const { isFetching, error, data } = useQuery({
         queryKey: ['coins'],
         queryFn: async () => {
             const response = await fetch('http://localhost:3001/api/coins');
-            console.log(data);
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             return response.json();
         }
     });
+
+    useEffect(() => {
+        if (data) {
+            setCoinsData(data);
+        }
+    }, [data]);
+
+    const handleSort = (e) => {
+        const column = e.target.innerText.toLowerCase();
+        const sortOrderCopy = { ...sortOrder };
+
+        // Toggle sort order
+        sortOrderCopy[column] = sortOrderCopy[column] === 'asc' ? 'desc' : 'asc';
+
+        const sortedHeads = [...data].sort((a, b) => {
+            if (column === 'rank') {
+                return sortOrderCopy[column] === 'asc' ? a.market_cap_rank - b.market_cap_rank : b.market_cap_rank - a.market_cap_rank;
+            } else if (column === 'name') {
+                return sortOrderCopy[column] === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
+            } else if (column === 'price') {
+                return sortOrderCopy[column] === 'asc' ? a.current_price - b.current_price : b.current_price - a.current_price;
+            } else if (column === '24hr') {
+                return sortOrderCopy[column] === 'asc' ? a.price_change_percentage_24h - b.price_change_percentage_24h : b.price_change_percentage_24h - a.price_change_percentage_24h;
+            } else if (column === 'volume') {
+                return sortOrderCopy[column] === 'asc' ? a.total_volume - b.total_volume : b.total_volume - a.total_volume;
+            } else if (column === 'market cap') {
+                return sortOrderCopy[column] === 'asc' ? a.market_cap - b.market_cap : b.market_cap - a.market_cap;
+            }
+        });
+
+        setSortOrder(sortOrderCopy);
+        setCoinsData(sortedHeads);
+    }
+
 
     if (isFetching) return <h1>Loading...</h1>;
 
@@ -43,17 +73,17 @@ const CoinTable = ({ searchQuery }) => {
                     <thead className="bg-gray-500 border">
                         <tr>
                             <th><IoIosStarOutline /></th>
-                            <th>Rank</th>
-                            <th>Name</th>
-                            <th>Price</th>
-                            <th>24hr</th>
-                            <th>Volume</th>
-                            <th>Market Cap</th>
-                            <th>Last 7 Days</th>
+                            <th onClick={handleSort}>Rank</th>
+                            <th onClick={handleSort}>Name</th>
+                            <th onClick={handleSort}>Price</th>
+                            <th onClick={handleSort}>24hr</th>
+                            <th onClick={handleSort}>Volume</th>
+                            <th onClick={handleSort}>Market Cap</th>
+                            <th onClick={handleSort}>Last 7 Days</th>
                         </tr>
                     </thead>
                     <tbody>
-                        { data && data.filter(coin => coin.name.toLowerCase().includes(searchQuery.toLowerCase())).map((coin) => {
+                        { coinsData && coinsData.filter(coin => coin.name.toLowerCase().includes(searchQuery.toLowerCase())).map((coin) => {
                             const priceIncrease = coin.sparkline_in_7d.price[coin.sparkline_in_7d.price.length - 1] > coin.sparkline_in_7d.price[0];
                             const isActive = activeStates[coin.id];
                             
@@ -63,8 +93,11 @@ const CoinTable = ({ searchQuery }) => {
                                         {isActive ? <IoMdStar className="text-yellow-500" /> : <IoIosStarOutline />}
                                     </td>
                                     <td>{coin.market_cap_rank}</td>
-                                    <td className="underline hover:text-blue-500">
-                                        <Link to={coin.id}><img className='h-6 align-middle' src={coin.image} alt={coin.name} />{coin.name}</Link>
+                                    <td className="underline hover:text-blue-500 w-28">
+                                        <Link to={coin.id} className="flex items-center justify-center">
+                                            <img className='h-6 align-middle mr-2' src={coin.image} alt={coin.name} />
+                                            <span>{coin.name}</span>
+                                        </Link>
                                     </td>
                                     <td className="">${(coin.current_price).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</td>
                                     <td style={{ color: coin.price_change_percentage_24h > 0 ? 'green' : 'red' }}>
